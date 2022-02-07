18d72b225a457cd78b490ec33c4ead1a747f4f89
Dalton <slattery.da@gmail.com>
2022-02-07 17:00:42 -0600

    commit tree 8ebe7b940d9089187c71fa1a5d0e36e2c6cc4c80

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App(){
+    const [imgSource, setImgSource] = useState('')
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(r=>r.json())
+            .then(data => setImgSource(data.message))
+    }, [])
+
+    if (imgSource === ''){
+        return <p>Loading...</p>
+    }
+
+    return <img src = {imgSource} alt ={imgSource} />
+}
+
+export default App
